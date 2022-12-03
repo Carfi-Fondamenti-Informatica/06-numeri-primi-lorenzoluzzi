0419614d9c8ac305fdfb64efdd70dc848f5801be
lorenzoluzzi <117952492+lorenzoluzzi@users.noreply.github.com>
2022-12-03 01:26:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
 
 int main(){
+  int h;
+  std::cin>>h;
+  if (numeroprimo(h)==0){
+    std::cout<<"numeroprimo"<<
+     std::endl;
+  }
+  else{
+    std::cout<<"numerononprimo"<<std::endl;
+  }
   return 0;
 }
