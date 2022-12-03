3dc76958ebb6cdcdd750874503892afab87731bd
lorenzoluzzi <117952492+lorenzoluzzi@users.noreply.github.com>
2022-12-03 01:32:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,11 +5,10 @@ int main(){
   int h;
   std::cin>>h;
   if (numeroprimo(h)==0){
-    std::cout<<"numeroprimo"<<
-     std::endl;
+    std::cout<<"numero primo"<< std::endl;
   }
   else{
-    std::cout<<"numerononprimo"<<std::endl;
+    std::cout<<"numero non primo"<< std::endl;
   }
   return 0;
 }
