a71ba57dcec6af29733bd1d0dc464afbedc612a3
lorenzoluzzi <117952492+lorenzoluzzi@users.noreply.github.com>
2022-12-03 01:29:21 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,22 @@
-
+#include <iostream>
+#include "lib.h"
+using namespace std;
+bool numeroprimo (int n) {
+    int i=1, x;
+    bool risultato;
+    if (n>1) {
+        do {
+            i++;
+            x = n%i;
+        }
+        while (x!=0);
+        if (n == i) {
+            risultato=0;
+            return risultato;
+        }
+        else {
+            risultato =1;
+            return risultato;
+        }
+    }
+}
